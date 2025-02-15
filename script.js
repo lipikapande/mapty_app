8c48177e58ec979d56f2ac697082e336904ce45f
Lipika <lipikapande@gmail.com>
2025-02-15 17:09:56 +0530

    geolocation

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
+    // console.log(position);
+    // console.log(longitude, latitude);
+    const map = L.map('map').setView(coords, 13);
+    //L is the main function that leaflet gives us
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords).addTo(map).bindPopup('Current location').openPopup();
+    map.on('click', function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng]).addTo(map).bindPopup(L.popup({})).openPopup();
+    });
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
